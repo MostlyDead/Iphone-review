c4072792c7029611b47808c9197eff112cb4ee13
Mostly Dead <mostlydead@mostly.no>
2021-03-22 18:12:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function myFunction(){
     alert("Hello!");
     dodument.body.style.background-color;"green"
-    window.open("https://repl.it/languages/python3");
-}
+    window.open('https://www.youtube.com/watch?v=0S6pHex-KCo','_blank') }
     
