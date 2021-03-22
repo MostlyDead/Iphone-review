7d5362bde037e4284ebe3353fa2f5cb181a53e47
Mostly Dead <62814499+MostlyDead@users.noreply.github.com>
2021-03-22 14:37:54 +0100

    Add files via upload

---
+++ alert.knapp.js
@@ -0,0 +1,19 @@
+function myFunction() {
+window.open('https://www.youtube.com/watch?v=0S6pHex-KCo','_blank')  
+}
+
+
+function alertColor() {
+
+const phoneColor = ['red', 'gree', 'blue', 'red']
+
+let randomNumber = Math.random() * phoneColor.length
+
+let newColor = Math.floor(randomNumber)
+
+let randomItem = shopItems[newColor]
+  }
+
+function colorAlert(){
+    alert(randomNumber)
+}
